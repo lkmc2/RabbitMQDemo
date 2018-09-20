dadc110960c91be1b3dd661a7377fd06803808f9
lkmc2 <lkmc2@163.com>
2018-09-20 18:08:31 +0800

    初始化项目

---
+++ src/main/java/com/lin/RabbimqApplication.java
@@ -0,0 +1,12 @@
+package com.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbimqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbimqApplication.class, args);
+    }
+}
